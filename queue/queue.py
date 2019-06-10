a53b4eb61d98ee39b01da15f7a8c81efb248bd81
Erin Koen <erin.koen@gmail.com>
2019-06-10 17:22:47 -0400

    queue working and WAY less complicated than I initially thought

--- queue/queue.py
+++ queue/queue.py
@@ -1,20 +1,35 @@
 '''
 Understand
 ----------
-A queue is an ordered list
+A queue is an ordered list. They're FIFO. Items get added to the back of the list, and taken off the front.
+- dequeueing == changing the second to last item in the list's next reference to Null.
+- enqueing == adding a new node with a next reference to the current head.
+- need to control for insertion? In other words, find a way to explicitly disallow it?
+- what happens when there's only one node? Or how do you instantiate a queue form nothing? What's the first node?
+
+Plan
+----
+- create a Node class, create a LinkedList Class, then create Queue as a subclass of LinkedList with the QUeue-specific functions
+
+*** IGNORE ALL THAT, DO IT AS A LIST***
+
+
 '''
-class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+class Queue():
+    def __init__(self):
+        self.size = 0
+        self.storage = []
 
-  def len(self):
-    pass
+
+    def enqueue(self, item):
+        self.storage.insert(0, item)
+        
+    def dequeue(self):
+        if len(self.storage) == 0:
+            return None
+        else:
+            return self.storage.pop()
+
+    def len(self):
+        return len(self.storage)
