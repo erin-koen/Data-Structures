1efdc16395c67a6d38f7456a3cd1a79581566a6d
Erin Koen <erin.koen@gmail.com>
2019-06-12 17:00:53 -0400

    everyting but for_each working on BST

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -15,7 +15,20 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    # recieves an index, which is where the value currently is
+    # keep bubbling up till we've reached the top of the heap or we've reached a point where the parent is higher priority.
+    # on a single bubble up iteration, we are going to get the parent index, compare the child index's value against teh value of the parent.
+    # if the child value is higher priority, then swap them, else child is at valid spot, stop bubbling. 
+    while index > 0:
+        parent = (index-1) // 2
+        if self.storage[index] > self.storage[parent]:
+            self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+            #update the child's index to reflect swap
+            index = parent
+        # otherwise the child is at a valid index.    
+        else: 
+            break
+
 
   def _sift_down(self, index):
     pass
