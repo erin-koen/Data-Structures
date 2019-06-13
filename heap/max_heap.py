f41765b838702740b95a2c8a4ae24cd5ec26251e
Erin Koen <erin.koen@gmail.com>
2019-06-13 16:13:37 -0400

    max heap sift down drafted and commented

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,34 +1,49 @@
 class Heap:
-  def __init__(self):
-    self.storage = []
+    def __init__(self):
+        self.storage = []
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        pass
 
-  def delete(self):
-    pass
+    def delete(self):
+        pass
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def get_size(self):
-    pass
+    def get_size(self):
+        pass
 
-  def _bubble_up(self, index):
-    # recieves an index, which is where the value currently is
-    # keep bubbling up till we've reached the top of the heap or we've reached a point where the parent is higher priority.
-    # on a single bubble up iteration, we are going to get the parent index, compare the child index's value against teh value of the parent.
-    # if the child value is higher priority, then swap them, else child is at valid spot, stop bubbling. 
-    while index > 0:
-        parent = (index-1) // 2
-        if self.storage[index] > self.storage[parent]:
-            self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
-            #update the child's index to reflect swap
-            index = parent
-        # otherwise the child is at a valid index.    
-        else: 
-            break
+    def _bubble_up(self, index):
+        # recieves an index, which is where the value currently is
+        # keep bubbling up till we've reached the top of the heap or we've reached a point where the parent is higher priority.
+        # on a single bubble up iteration, we are going to get the parent index, compare the child index's value against teh value of the parent.
+        # if the child value is higher priority, then swap them, else child is at valid spot, stop bubbling.
+        while index > 0:
+            parent = (index-1) // 2
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+                # update the child's index to reflect swap
+                index = parent
+            # otherwise the child is at a valid index.
+            else:
+                break
 
+    def _sift_down(self, index):
 
-  def _sift_down(self, index):
-    pass
+        # declare the children
+        i = index
+        right = 2*i+1
+        left = 2*i+2
+        # while there are children
+        while self.storage[right] is not None or self.storage[left] is not None:
+        # if the item at storage[index] is less than either of its children, swap it with the larger of the two.
+            if self.storage[i] < self.storage[left] or self.storage[i] < self.storage[right]:
+                if self.storage[left] > self.storage[right]:
+                    self.storage[i], self.storage[left] = self.storage[left], self.storage[i]
+                    i = left
+                else: 
+                    self.storage[i], self.storage[right] = self.storage[right], self.storage[i]
+                    i = right
+            else:
+                break
